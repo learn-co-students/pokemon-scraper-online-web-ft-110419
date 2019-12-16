bb89c70125854e721babbef6c0f1d05bf912fe12
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-16 18:28:12 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
+require "pry"
 class Pokemon
+  attr_accessor :name, :type, :db ,:id 
+  def initialize(id:, name:, type:,db:)
+    @id
+    @name
+    @type
+    @db
+  end
+  def self.save(name,type,db)
+    sql = <<-SQL
+     INSERT INTO pokemon(name,type) values (?,?)
+    SQL
+    db.execute(sql,name,type)
+  end
+  def self.find(id,db)
+    
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id = ? LIMIT 1",id).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+    
+  end
 end
